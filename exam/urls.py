a8df010a29f6dfacb2a8bf3192e8181781f16fa0
LugnaDash3 <lugnadash3@gmail.com>
2026-04-10 23:37:20 +0530

    Update urls.py

--- exam/urls.py
+++ exam/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.course_detail, name='course_detail'),
     path('courses/<int:course_id>/', views.course_detail, name='course_detail_with_id'),
-    path('lessons/<int:lesson_id>/submit/', views.submit, name='submit'),
-    path('submissions/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+    path('submit/', views.submit, name='submit'),
+    path('show_exam_result/', views.show_exam_result, name='show_exam_result'),
 ]
